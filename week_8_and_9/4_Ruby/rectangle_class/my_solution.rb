a76eee081334173f60d6e6e2a4eb38a885dc106a
Rick Dsida <rick.dsida@icloud.com>
2014-05-12 00:50:36 -0500

    Rectangle and reverse

--- week_8_and_9/4_Ruby/rectangle_class/my_solution.rb
+++ week_8_and_9/4_Ruby/rectangle_class/my_solution.rb
@@ -1,7 +1,7 @@
 # U3.W8-9: Implement a Rectangle Class
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 2. Pseudocode
 
@@ -15,6 +15,22 @@ class Rectangle
     @width  = width
     @height = height
   end
+  def area
+  	width*height
+  end
+  def perimeter
+  	2*width+2*height
+  end
+  def diagonal
+  	Math.sqrt((width**2)+(height**2))
+  end
+  def square?
+  	if width==height
+  		true
+  	else
+  		false
+  	end
+  end
 
   def ==(other)
     (other.width  == self.width && other.height == self.height ) ||
@@ -23,20 +39,35 @@ class Rectangle
 end
 
 
-
-
-# 4. Refactored Solution
-
-
-
-
-
-
 # 1. DRIVER TESTS GO BELOW THIS LINE
+def assert
+	raise "failed" unless yield
+end
 
+test1 = Rectangle.new(50,100)
+test2 = Rectangle.new(10,10)
+test3 = Rectangle.new(30,40)
+
+assert{test1.area==5000}
+assert{test2.area==100}
+assert{test3.area==1200}
+
+assert{test1.perimeter==300}
+assert{test2.perimeter==40}
+assert{test3.perimeter==140}
+
+assert{test1.diagonal==Math.sqrt((50**2)+(100**2))}
+
+assert{test1.square? == false}
+assert{test2.square? == true}
+assert{test3.square? == false}
 
 
 
 
 
 # 5. Reflection 
+
+# I enjoyed working with the rectangle class it was fairly simple the main issue I encountered was with 
+# the driver code. For the diagonal driver code required a correct answer.  I ended up including an equation
+# in the driver code so that i didn't have to deal with rounding or leaving off a decimal place
--- week_8_and_9/4_Ruby/reverse_words/my_solution.rb
+++ week_8_and_9/4_Ruby/reverse_words/my_solution.rb
@@ -1,29 +1,32 @@
 # U3.W8-9: Reverse Words
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 2. Pseudocode
-
-
-
-# 3. Initial Solution
 
 
 
 
 # 4. Refactored Solution
 
-
+def reverse(words)
+	new = words.split.map {|word| word.reverse}
+	new.join(' ')
+end
 
 
 
 
 # 1. DRIVER TESTS/ASSERT STATEMENTS GO BELOW THIS LINE
 
-
-
+p reverse("This is reversed") == "sihT si desrever"
+p reverse("I'm a leaf on the wind") == "m'I a fael no eht dniw"
+p reverse("Let's go Hawks") == "s'teL og skwaH"
 
 
 
 # 5. Reflection 
+
+# This was a very quick exercise I was able to complete fairly quickly. I didnt have to refactor the code because my first 
+# solution was pretty clear and concise
